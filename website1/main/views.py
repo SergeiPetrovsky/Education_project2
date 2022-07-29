4644857a079f48b6ea3ed370d28aa21da264e164
Сергей <petrovskysergei72@gmail.com>
2022-07-29 16:32:11 +0300

    stage3

--- website1/main/views.py
+++ website1/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Task
 
 def index(request):
-    task = Task.objects.all()
+    task = Task.objects.order_by("-created_on")
     parameters = {'title': 'Главная страница',
                   'tasks': task
                   }
@@ -20,3 +20,6 @@ def page2(request):
 
 def page3(request):
     return render(request, 'page3.html')
+
+def create(request):
+    return render(request, 'create.html')
